482afe1e4f389d92f1325abb2edfcbc090244d74
jkwok0714 <jkwok0714@gmail.com>
2017-08-08 17:01:18 -0700

    Moved script to .js, cleanup

---
+++ main.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    var $body = $('#scrollbox');
+    var autoRefreshEnabled = true; //Tracks if the tweets will update
+    var spotlightUser = ''; //Tracks if a user is highlighted
+
+    function refreshTweets() {
+
+        var index = 0;
+        while (index <= streams.home.length - 1) {
+            var tweet = streams.home[index];
+            var $tweet = $('<div class="userTweet ' + tweet.user + '"></div>');
+            $tweet.html('@<button class="userButton">' + tweet.user + '</button>: <span class="tweet">' +
+                tweet.message + '</span><br><span class="timestamp">' + tweet.created_at + '</span>');
+            $tweet.attr('id', 'tweet' + index);
+            if ($('#tweet' + index).length === 0) $tweet.prependTo($body);
+            index += 1;
+        }
+        if (spotlightUser != "") showSpotlightUser(0);
+    }
+
+    function showSpotlightUser(speed) {
+        $('.userTweet').not('.' + spotlightUser).hide(speed);
+    }
+
+    function showAllUsers(speed) {
+        spotlightUser = '';
+        $('.userTweet').show(speed);
+    }
+
+    function clearTweets() {
+        $('.userTweet').remove();
+    }
+
+    //Initial refresh and interval
+    refreshTweets();
+    setInterval(function() {
+        if (autoRefreshEnabled) refreshTweets();
+    }, 10000);
+
+    //Manual refresh
+    $('#refreshButton').on('click', refreshTweets);
+
+    //Toggle auto refresh
+    $('#toggleRefreshButton').on('click', function() {
+        autoRefreshEnabled = !autoRefreshEnabled;
+		if (autoRefreshEnabled) {
+			$('#toggleRefreshButton').removeClass('selectToggle');
+			$('#toggleRefreshButton').text('Disable Auto-Refresh');
+		} else {
+			$('#toggleRefreshButton').addClass('selectToggle');
+			$('#toggleRefreshButton').text('Enable Auto-Refresh');
+		}
+        $('#autoRefreshStatus').text('Auto-Refresh: ' + autoRefreshEnabled);
+    });
+
+
+    //User buttons
+    $('#scrollbox').on('click', '.userButton', function() {
+        var user = $(this).text();
+        if (spotlightUser != '') {
+            showAllUsers(200);
+        } else {
+            spotlightUser = user;
+            showSpotlightUser(200);
+        }
+
+    });
+
+});
